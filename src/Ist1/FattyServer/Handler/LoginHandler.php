b64a33f50d0b14f96b7f7ee444f65eb36057a6e7
Istvan Jano <janoist1@gmail.com>
2014-04-05 23:18:00 +0100

    Updated logic in LoginHandler, now it sends the TableList after Login.

--- src/Ist1/FattyServer/Handler/LoginHandler.php
+++ src/Ist1/FattyServer/Handler/LoginHandler.php
@@ -8,6 +8,7 @@ use FattyServer\Packet\Input\Login as LoginIn;
 use FattyServer\Packet\Output\Login as LoginOut;
 use FattyServer\Packet\Output\NewPlayer;
 use FattyServer\Packet\Output\PlayersList;
+use FattyServer\Packet\Output\TablesList;
 
 
 class LoginHandler implements HandlerInterface
@@ -29,18 +30,20 @@ class LoginHandler implements HandlerInterface
      * Handles login request.
      *
      * @param FattyConnection $fattyConnFrom
+     * @param FattyServerProtocol $serverProtocol
      */
     public function handle(FattyConnection $fattyConnFrom, FattyServerProtocol $serverProtocol)
     {
         $player = $serverProtocol->getPlayerManager()->createAndAddPlayer(
             $fattyConnFrom, $this->packet->getName()
         );
+        $tables = $serverProtocol->getTableManager()->getTables();
 
         $fattyConnFrom->sendPacket(new LoginOut());
+        $fattyConnFrom->sendPacket(new TablesList($tables));
         $fattyConnFrom->sendPacket(
             new PlayersList($serverProtocol->getPlayerManager()->getPlayers())
         );
-
         $serverProtocol->getPropagator()->sendPacketToPlayers(
             new NewPlayer($player),
             $fattyConnFrom
